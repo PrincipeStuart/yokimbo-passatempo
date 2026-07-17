7aa921333d9dceb9b1949a91d1266a3b50c0b245
PrincipeStuart <96055779+PrincipeStuart@users.noreply.github.com>
2026-07-17 21:37:56 +0100

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,76 +1,37 @@
 
-/* ================================================
-   FIREBASE CONFIGURATION - Yokimbo Passatempo
-   ================================================ */
-
-/*
-    Configuração do Firebase
-
-    Este arquivo conecta o site ao Firestore.
-
-    Não contém dados sensíveis.
-    As regras de segurança do Firebase
-    controlam o acesso aos dados.
-*/
-
+// ==========================================
+// FIREBASE CONFIGURATION - YOKIMBO PASSATEMPO
+// ==========================================
 
 // Importações Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 import { 
-    getFirestore,
-    collection,
-    addDoc,
-    serverTimestamp,
-    query,
-    where,
-    getDocs
+    getFirestore 
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 
-// Configuração do projeto Yokimbo
+// Configuração Firebase Yokimbo
 const firebaseConfig = {
-
     apiKey: "AIzaSyBsjukcvyy8v632eMrlagOK01fFRjvcGwE",
-
-    authDomain:
-    "yokimbo-passatempo.firebaseapp.com",
-
-    projectId:
-    "yokimbo-passatempo",
-
-    storageBucket:
-    "yokimbo-passatempo.firebasestorage.app",
-
-    messagingSenderId:
-    "478275775549",
-
-    appId:
-    "1:478275775549:web:74479f3bb583ecb8c6ce54"
+    authDomain: "yokimbo-passatempo.firebaseapp.com",
+    projectId: "yokimbo-passatempo",
+    storageBucket: "yokimbo-passatempo.firebasestorage.app",
+    messagingSenderId: "478275775549",
+    appId: "1:478275775549:web:74479f3bb583ecb8c6ce54"
 };
 
 
 // Inicializar Firebase
-
-const firebaseApp = initializeApp(firebaseConfig);
-
-
-// Inicializar Firestore
-
-const db = getFirestore(firebaseApp);
+const app = initializeApp(firebaseConfig);
 
 
-// Exportar funções
-
-export {
-    db,
-    collection,
-    addDoc,
-    serverTimestamp,
-    query,
-    where,
-    getDocs
-};
+// Criar ligação Firestore
+const db = getFirestore(app);
 
 
-console.log("🔥 Firebase conectado com sucesso!");
+// Exportar para outros ficheiros
+export { db };
+
+
+console.log("🔥 Firebase conectado com sucesso");

98759c44906c800be8fab1a6a45d2fe2a6b0ff55
PrincipeStuart <96055779+PrincipeStuart@users.noreply.github.com>
2026-07-17 23:39:09 +0100

    Update admin-auth.js

--- js/admin-auth.js
+++ js/admin-auth.js
@@ -1,1 +1,67 @@
+// ==================================================
+// ADMIN-AUTH.JS
+// Proteção do painel administrativo Yokimbo
+// ==================================================
 
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+
+import {
+    getAuth,
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyBsjukcvyy8v632eMrlagOK01fFRjvcGwE",
+
+    authDomain: "yokimbo-passatempo.firebaseapp.com",
+
+    projectId: "yokimbo-passatempo",
+
+    storageBucket: "yokimbo-passatempo.firebasestorage.app",
+
+    messagingSenderId: "478275775549",
+
+    appId: "1:478275775549:web:74479f3bb583ecb8c6ce54"
+
+};
+
+
+
+const app = initializeApp(firebaseConfig);
+
+
+const auth = getAuth(app);
+
+
+
+onAuthStateChanged(
+    auth,
+    (user)=>{
+
+
+        if(!user){
+
+
+            window.location.href =
+            "login.html";
+
+
+        } else {
+
+
+            console.log(
+                "✅ Acesso autorizado:",
+                user.email
+            );
+
+
+        }
+
+
+    }
+);
